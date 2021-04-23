e320989f601527b0c416e8840d9ce42b5ba96f83
kangilbin <69296105+kangilbin@users.noreply.github.com>
2021-04-23 12:09:49 +0900

    슬라이드 추가

---
+++ screens/Movies/MoviesPresenter.js
@@ -0,0 +1,35 @@
+import React from "react";
+import styled from "styled-components/native";
+import Swiper from "react-native-web-swiper";
+import { ActivityIndicator, Dimensions } from "react-native";
+
+const {width, height} = Dimensions.get("screen");
+
+const Container = styled.View`
+    flex:1;
+    background-color:black;
+    justify-content: center;
+`;
+const Header = styled.View`
+    width:100%;
+    height:${height/3}px
+`;
+const Section = styled.View`
+    background-color: red;
+    height: 100%
+`;
+const Text = styled.Text``;
+export default ({loading, nowPlaying}) => (
+    <Container>
+       {loading ? <ActivityIndicator color="white" size="samll" /> : ( <Header>
+            <Swiper controlsEnabled={false} loop timeout={3}>
+                {nowPlaying.map(movie => (
+                <Section key={movie.id}>
+                    <Text>{movie.original_title}</Text>
+                </Section>
+                ))}
+            </Swiper>
+        </Header>
+    )}
+    </Container>
+);
